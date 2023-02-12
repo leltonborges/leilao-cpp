3897215e43f9035eb5890ed14eab7110f0756133
Lelton Borges <lelton.borges@fornecedores.sicoob.com.br>
2023-02-11 23:27:11 -0300

    chore: move constructor

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <memory>
 #include "Usuario.hpp"
 
@@ -10,15 +10,64 @@ using namespace std;
     return std::malloc(bytes);
 }*/
 
-void showNameUser(const std::unique_ptr<Usuario> user){
+class String {
+private:
+    char *data;
+    size_t size;
+public:
+    explicit String(const char *string) {
+        std::cout << "Criando string" << std::endl;
+        size = strlen(string);
+        data = new char[size];
+        std::memcpy(data, string, size);
+    }
+
+    ~String() {
+        delete data;
+    }
+
+    String(const String &other) {
+        std::cout << "copy string" << std::endl;
+        size = std::strlen(other.data);
+        data = new char[size];
+        std::memcpy(data, other.data, size);
+    }
+
+    String(String &&other) noexcept(false) {
+        std::cout << "move string" << std::endl;
+        size = other.size;
+        data = other.data;
+
+        other.size = 0;
+        other.data = nullptr;
+    }
+};
+
+class User {
+private:
+    String name;
+public:
+    explicit User(const String &string):
+            name(string) {}
+
+    explicit User(const String &&string):
+            name((String &&) string) {}
+};
+
+void showNameUser(const std::unique_ptr<Usuario> user) {
     cout << "Show Name user: " << user->getName() << endl;
 }
 
-void showNameUser(const std::shared_ptr<Usuario> user){
+void showNameUser(const std::shared_ptr<Usuario> user) {
     cout << "Show Name user: " << user->getName() << endl;
 }
 
-int main2() {
+int &getNumber() {
+    static int value = 10;
+    return value;
+}
+
+int main() {
     std::cout << "---------------" << endl;
     std::string fulano = "Fulano Acala de Tal beutrano & Ciclano da Abete das canvas";
     std::string_view beutrano(fulano.c_str(), fulano.find('&') - 1);
@@ -31,6 +80,10 @@ int main2() {
     std::shared_ptr<Usuario> user2 = std::make_shared<Usuario>(Usuario("Fulano de tal Beutrano"));
 
     showNameUser(user2);
+    std::cout << "Number: " << getNumber() << std::endl;
+    getNumber() = 78;
+    std::cout << "Number: " << getNumber() << std::endl;
 
+    User(String("Fulano de tal"));
     return 0;
 }
